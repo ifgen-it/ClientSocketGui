b305437257121974940c3287be0d04741557bd7f
Evgen Smirnov <ifgen.it@gmail.com>
2019-02-14 22:59:47 +0300

    Add project files.

---
+++ ClientSocketGui/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace ClientSocketGui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.tbChat = new System.Windows.Forms.TextBox();
+            this.tbOnlineUsers = new System.Windows.Forms.TextBox();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.lbInfo = new System.Windows.Forms.Label();
+            this.lbServer = new System.Windows.Forms.Label();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.lbOnlineUsers = new System.Windows.Forms.Label();
+            this.lbUser = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnLogOut = new System.Windows.Forms.Button();
+            this.tipRefresh = new System.Windows.Forms.ToolTip(this.components);
+            this.tipSend = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tbMessage.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbMessage.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.tbMessage.Location = new System.Drawing.Point(12, 354);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbMessage.Size = new System.Drawing.Size(672, 49);
+            this.tbMessage.TabIndex = 3;
+            this.tbMessage.Text = "Type message here...";
+            // 
+            // tbChat
+            // 
+            this.tbChat.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tbChat.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbChat.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.tbChat.Location = new System.Drawing.Point(12, 80);
+            this.tbChat.Multiline = true;
+            this.tbChat.Name = "tbChat";
+            this.tbChat.ReadOnly = true;
+            this.tbChat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbChat.Size = new System.Drawing.Size(478, 268);
+            this.tbChat.TabIndex = 0;
+            this.tbChat.TabStop = false;
+            this.tbChat.Text = "Chat will be here";
+            // 
+            // tbOnlineUsers
+            // 
+            this.tbOnlineUsers.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tbOnlineUsers.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbOnlineUsers.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.tbOnlineUsers.Location = new System.Drawing.Point(496, 80);
+            this.tbOnlineUsers.Multiline = true;
+            this.tbOnlineUsers.Name = "tbOnlineUsers";
+            this.tbOnlineUsers.ReadOnly = true;
+            this.tbOnlineUsers.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbOnlineUsers.Size = new System.Drawing.Size(188, 268);
+            this.tbOnlineUsers.TabIndex = 0;
+            this.tbOnlineUsers.TabStop = false;
+            this.tbOnlineUsers.Text = "Users";
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLogIn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnLogIn.Location = new System.Drawing.Point(310, 26);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(180, 32);
+            this.btnLogIn.TabIndex = 2;
+            this.btnLogIn.Text = "Log in";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            // 
+            // lbInfo
+            // 
+            this.lbInfo.AutoSize = true;
+            this.lbInfo.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbInfo.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbInfo.Location = new System.Drawing.Point(16, 483);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(72, 19);
+            this.lbInfo.TabIndex = 0;
+            this.lbInfo.Text = "Info : ";
+            // 
+            // lbServer
+            // 
+            this.lbServer.AutoSize = true;
+            this.lbServer.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbServer.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbServer.Location = new System.Drawing.Point(16, 453);
+            this.lbServer.Name = "lbServer";
+            this.lbServer.Size = new System.Drawing.Size(288, 19);
+            this.lbServer.TabIndex = 0;
+            this.lbServer.Text = "Server : 192.168.777.777 : 8888";
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tbUserName.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbUserName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.tbUserName.Location = new System.Drawing.Point(124, 26);
+            this.tbUserName.Multiline = true;
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(180, 30);
+            this.tbUserName.TabIndex = 1;
+            this.tbUserName.Text = "User name";
+            // 
+            // lbOnlineUsers
+            // 
+            this.lbOnlineUsers.AutoSize = true;
+            this.lbOnlineUsers.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbOnlineUsers.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbOnlineUsers.Location = new System.Drawing.Point(529, 33);
+            this.lbOnlineUsers.Name = "lbOnlineUsers";
+            this.lbOnlineUsers.Size = new System.Drawing.Size(117, 19);
+            this.lbOnlineUsers.TabIndex = 0;
+            this.lbOnlineUsers.Text = "Online users";
+            // 
+            // lbUser
+            // 
+            this.lbUser.AutoSize = true;
+            this.lbUser.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbUser.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbUser.Location = new System.Drawing.Point(30, 33);
+            this.lbUser.Name = "lbUser";
+            this.lbUser.Size = new System.Drawing.Size(72, 19);
+            this.lbUser.TabIndex = 0;
+            this.lbUser.Text = "User >_";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSend.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnSend.Location = new System.Drawing.Point(12, 409);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(220, 32);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Enabled = false;
+            this.btnRefresh.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRefresh.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnRefresh.Location = new System.Drawing.Point(238, 409);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(220, 32);
+            this.btnRefresh.TabIndex = 5;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.Enabled = false;
+            this.btnLogOut.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLogOut.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnLogOut.Location = new System.Drawing.Point(464, 409);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(220, 32);
+            this.btnLogOut.TabIndex = 6;
+            this.btnLogOut.Text = "Log out";
+            this.btnLogOut.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(696, 515);
+            this.Controls.Add(this.btnLogOut);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.tbChat);
+            this.Controls.Add(this.tbOnlineUsers);
+            this.Controls.Add(this.tbUserName);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.lbServer);
+            this.Controls.Add(this.lbUser);
+            this.Controls.Add(this.lbOnlineUsers);
+            this.Controls.Add(this.lbInfo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(712, 553);
+            this.MinimumSize = new System.Drawing.Size(712, 553);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chatiko";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.TextBox tbChat;
+        private System.Windows.Forms.TextBox tbOnlineUsers;
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.Label lbInfo;
+        private System.Windows.Forms.Label lbServer;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.Label lbOnlineUsers;
+        private System.Windows.Forms.Label lbUser;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnLogOut;
+        private System.Windows.Forms.ToolTip tipRefresh;
+        private System.Windows.Forms.ToolTip tipSend;
+    }
+}
+
---
+++ ClientSocketGui/Form1.cs
@@ -0,0 +1,400 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace ClientSocketGui
+{
+    public partial class Form1 : Form
+    {
+        // fields
+
+        static int port = 8005;
+        static string address = "127.0.0.1";
+
+        static string getHistory = "0";
+        static string wantExit = "2";
+        static string whoIsOnline = "1";
+        static string messageDelivered = "+";
+
+        Socket socket = null;
+        bool sentName = false;
+        bool wasLogIn = false;
+        bool typeMessageNow = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // my initializations
+            InitServer();
+
+            // tool tips
+            tipRefresh.SetToolTip(btnRefresh, "F5");
+            tipSend.SetToolTip(btnSend, "Enter");
+
+            // handlers
+            btnLogIn.Click += BtnLogIn_Click;
+            btnSend.Click += BtnSend_Click;
+            btnRefresh.Click += BtnRefresh_Click;
+            btnLogOut.Click += BtnLogOut_Click;
+
+            tbMessage.GotFocus += (o, e) => { typeMessageNow = true; };
+            tbMessage.LostFocus += (o, e) => { typeMessageNow = false; };
+
+        }
+
+        private void BtnLogOut_Click(object sender, EventArgs e)
+        {
+            LogOut();
+        }
+
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshChat();
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            SendMessage();
+        }
+
+        private void BtnLogIn_Click(object sender, EventArgs e)
+        {
+            LogIn();
+        }
+
+
+
+
+
+        // methods
+
+        private void UpdateInfo(string text)
+        {
+            lbInfo.Text = "Info : " + text;
+        }
+
+        private void UpdateServerInfo()
+        {
+            lbServer.Text = "Server : " + address + " : " + port;
+        }
+
+        private void InitServer()
+        {
+            // READ SERVER CONFIGURATION FROM FILE CONFIG.TXT
+            try
+            {
+                StreamReader rd = new StreamReader("client_config.txt");
+                string strServer = rd.ReadLine().Trim();
+                string strPort = rd.ReadLine().Trim();
+                rd.Close();
+
+                string[] strServers = strServer.Split(' ');
+                string[] strPorts = strPort.Split(' ');
+                port = int.Parse(strPorts[1]);
+                address = strServers[1];
+                UpdateInfo("Configuration loaded. Type user name and press 'Log in'");
+                UpdateServerInfo();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("\nCheck existance of original file client_config.txt in the directory with program.\n");
+
+                UpdateInfo("Error in the configuration file client_config.txt");
+            }
+
+        }
+
+        private bool InitSocket()
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(ipPoint);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                UpdateInfo("Server is not responding. Make sure that Server is running");
+                return false;
+            }
+            
+        }
+
+        private string GetAnswer()
+        {
+
+            //  GET ANSWER  
+            try
+            {
+                byte[] data = new byte[256];
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                } while (socket.Available > 0);
+
+                Console.WriteLine(DateTime.Now.ToLongTimeString() + " " + builder.ToString() + "\n");
+
+                string ret = builder.ToString(); 
+                return ret;
+
+            }
+            catch (Exception ex)
+            {
+                UpdateInfo(ex.Message);
+                return ex.Message;
+            }
+        }
+
+        private string GetTime()
+        {
+            return DateTime.Now.ToLongTimeString() + " ";
+        }
+
+        private void UpdateOnlineUsers()
+        {
+            try
+            {
+                string message = whoIsOnline;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                socket.Send(data);
+
+                string answer = GetAnswer();
+
+                string users = answer.TrimStart(char.Parse(messageDelivered));
+                users = users.TrimStart('\n');
+
+                tbOnlineUsers.Text = users;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                UpdateInfo("Server is not responding");
+            }
+            
+        }
+
+        private string NewLine()
+        {
+            return new StringBuilder().AppendLine().ToString();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            bool isHandled = true;
+
+
+            switch (keyData)
+            {
+
+                case Keys.F5:
+                    if (wasLogIn == true)
+                    {
+                        RefreshChat();
+                        return isHandled;
+                    }
+                    break;
+
+                case Keys.Enter:
+                    if (typeMessageNow == true)
+                    {
+                        SendMessage();
+                        return isHandled;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+
+            return false;
+        }
+
+        private void RefreshChat()
+        {
+            try
+            {
+                string message = getHistory;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                socket.Send(data);
+
+                string history = GetAnswer();
+
+                if (history.Length == 1 && history.Equals(messageDelivered))
+                {
+                    UpdateInfo("No messages from other users");
+                }
+                else
+                {
+                    if (history.Length > 1)
+                    {
+                        Console.WriteLine(history);
+                        char whatTrim = char.Parse(messageDelivered);
+                        history = history.Trim(whatTrim);
+                        history = history.TrimStart('\n');
+                        Console.WriteLine(history);
+
+                        tbChat.AppendText(history);
+                        //tbChat.ScrollToCaret();
+
+                        UpdateInfo("Messages from other users loaded");
+                    }
+                    else
+                    {
+                        UpdateInfo("Refresh error");
+                    }
+                }
+
+                UpdateOnlineUsers();
+            }
+            catch (Exception ex)
+            {
+                UpdateInfo(ex.Message);
+            }
+        }
+
+        private void SendMessage()
+        {
+            try
+            {
+                string message = tbMessage.Text.Trim();
+                if (message.Equals(""))
+                {
+                    UpdateInfo("Type message!");
+                    return;
+                }
+
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                socket.Send(data);
+
+                string answer = GetAnswer();
+
+                if (answer.Length == 1 && answer.Equals(messageDelivered))
+                {
+                    //tbChat.Text += GetTime() + message + NewLine();
+                    //tbChat.ScrollToCaret();
+                    tbChat.AppendText(GetTime() + message + NewLine());
+
+                    tbMessage.Text = "";
+                    tbMessage.Focus();
+                    UpdateInfo("Message was delivered");
+                }
+                else
+                {
+                    if (answer.Length > 1)
+                    {
+                        char whatTrim = char.Parse(messageDelivered);
+                        string history = answer.Trim(whatTrim);
+                        history = history.TrimStart('\n');
+                        Console.WriteLine(history);
+                        tbChat.AppendText(history);
+
+                        //tbChat.Text += GetTime() + message + NewLine();
+                        // tbChat.ScrollToCaret();
+                        tbChat.AppendText(GetTime() + message + NewLine());
+
+                        tbMessage.Text = "";
+                        tbMessage.Focus();
+                        UpdateInfo("History messages loaded. Message was delivered");
+                    }
+                    else
+                    {
+                        UpdateInfo("Message delivery error");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                UpdateInfo(ex.Message);
+            }
+        }
+
+        private void LogOut()
+        {
+            try
+            {
+                byte[] data = Encoding.Unicode.GetBytes(wantExit);
+                socket.Send(data);
+                Console.WriteLine("You was exited from the chat");
+                UpdateInfo("You was exited from the chat");
+
+                tbUserName.Enabled = true;
+                btnLogIn.Enabled = true;
+
+                // CLOSE SOCKET
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+
+                btnSend.Enabled = false;
+                btnRefresh.Enabled = false;
+                btnLogOut.Enabled = false;
+
+                wasLogIn = false;
+            }
+            catch (Exception ex)
+            {
+                UpdateInfo(ex.Message);
+            }
+        }
+
+        private void LogIn()
+        {
+            if (InitSocket() == false) return;
+
+            string name = tbUserName.Text.Trim();
+            if (name.Equals(""))
+            {
+                UpdateInfo("Type user name!");
+                return;
+            }
+            tbUserName.Enabled = false;
+            btnLogIn.Enabled = false;
+
+            try
+            {
+                byte[] dataName = Encoding.Unicode.GetBytes(name);
+                sentName = true;
+                socket.Send(dataName);
+            }
+            catch (Exception ex)
+            {
+                UpdateInfo(ex.Message);
+            }
+
+            tbChat.Text = "";
+            tbOnlineUsers.Text = "";
+            tbMessage.Text = "";
+
+            tbChat.Text += GetTime() + GetAnswer() + NewLine();
+            UpdateInfo("Type message and press 'Send'");
+
+            UpdateOnlineUsers();
+
+            btnSend.Enabled = true;
+            btnRefresh.Enabled = true;
+            btnLogOut.Enabled = true;
+            wasLogIn = true;
+        }
+
+
+
+    }
+}
